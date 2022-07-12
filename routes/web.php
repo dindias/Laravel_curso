9e9b434c8539122e998adfab44228cb5eebf3af3
dindias <dani.indias-corral@epitech.eu>
2022-07-12 10:47:27 +0200

    lesson 18

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,6 @@ Route::get('/', function() {
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::get('/portfolio', PortfolioController::class)->name('portfolio');
+Route::get('/portfolio', 'PortfolioController@index')->name('portfolio.index');
 
 Route::view('/contact', 'contact')->name('contact');
