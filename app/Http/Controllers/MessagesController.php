9b424bf5f2e01f726dc62eff83f8eb6bfd4e2ec2
dindias <dani.indias-corral@epitech.eu>
2022-07-13 10:31:59 +0200

    final video 19

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -7,7 +7,10 @@ class MessagesController extends Controller
     public function store()
     {
         request()->validate([
-            'name' => 'required'
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'content' => 'required|min:3'
         ]);
 
         return 'Datos validados';
