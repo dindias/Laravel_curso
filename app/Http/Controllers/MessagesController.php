d18975e695bcbf28f07336b2eb471441a9202c40
dindias <dani.indias-corral@epitech.eu>
2022-07-19 11:35:34 +0200

    commit viejo

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
+
+class MessagesController extends Controller
+{
+    public function store()
+    {
+        $message = request()->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'content' => 'required|min:3'
+        ]);
+
+        Mail::to('danielsandic117@gmail.com')->queue(new MessageReceived($message));
+
+        return 'Datos validados';
+    }
+}
